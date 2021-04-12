553ffde7a8aaaa7826fc73da69066d7d21a3531f
ajatprabha <ajat.prabha.leo@gmail.com>
2021-04-12 13:50:05 +0530

    update doc for Store interface

--- alias.go
+++ alias.go
@@ -6,6 +6,13 @@ import (
 
 // Store is an interface which can be used to provide implementations
 // for message persistence.
+//
+// [IMPORTANT]
+// When implementing a store with a shared storage (ex: redis) across multiple
+// application instances, it should be ensured that the keys are namespaced for
+// each application instance otherwise there will be collisions.
+// The messages are identified based on message id from the MQTT packets, and they have values
+// in range (0, 2^16), this coincides with max number of in-flight messages
 type Store = mqtt.Store
 
 // Message defines the externals that a message implementation must support.

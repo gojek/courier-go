1b912b48a6ac46cc319ebf75310f1b2fd4e58a6f
ajatprabha <ajat.prabha.leo@gmail.com>
2021-07-23 06:49:44 +0000

    [COURIER-437][Ajat] add webhook router to allow easy handling of events from kafka

--- doc.go
+++ doc.go
@@ -4,93 +4,89 @@ infrastructure to publish/subscribe to messages from other clients
 
 Example:
 
-```go
+	package main
 
-package main
+	import (
+		"context"
+		"fmt"
+		"net/http"
+		"os"
+		"os/signal"
+		"syscall"
+		"time"
 
-import (
-	"context"
-	"fmt"
-	"net/http"
-	"os"
-	"os/signal"
-	"syscall"
-	"time"
+		"github.com/prometheus/client_golang/prometheus"
+		"github.com/prometheus/client_golang/prometheus/promhttp"
 
-	"github.com/prometheus/client_golang/prometheus"
-	"github.com/prometheus/client_golang/prometheus/promhttp"
+		"***REMOVED***"
+		"***REMOVED***/metrics"
+	)
 
-	"***REMOVED***"
-	"***REMOVED***/metrics"
-)
-
-func main() {
-	reg := prometheus.NewRegistry()
-	m := metrics.NewPrometheus()
-	if err := m.AddToRegistry(reg); err != nil {
-		panic(err)
-	}
-
-	c, err := courier.NewClient(
-			courier.WithUsername("username"),
-			courier.WithPassword("password"),
-			courier.WithTCPAddress("localhost", 1883),
-			courier.WithKeepAlive(10*time.Second),
-			courier.WithConnectTimeout(10*time.Second),
-			courier.WithWriteTimeout(2*time.Second),
-			courier.WithMaxReconnectInterval(5*time.Minute),
-			courier.WithGracefulShutdownPeriod(time.Minute),
-			courier.WithCustomMetrics(m),
-			// courier.WithPersistence(s),   // persistence for qos > 0 use-cases
-		)
-
-	if err != nil {
-		panic(err)
-	}
-
-	metricsServer := http.Server{
-		Addr:              ":9090",
-		Handler:           promhttp.HandlerFor(reg, promhttp.HandlerOpts{}),
-	}
-	go func() {
-		_ = metricsServer.ListenAndServe()
-	}()
-
-	if err := c.Start(); err != nil {
-		panic(err)
-	}
-
-	stopCh := make(chan os.Signal)
-	signal.Notify(stopCh, []os.Signal{os.Interrupt, syscall.SIGTERM}...)
-
-	go func() {
-		tick := time.NewTicker(time.Second)
-		for {
-			select {
-			case t := <-tick.C:
-				msg := map[string]interface{}{
-					"time": t.UnixNano(),
-				}
-				if err := c.Publish(context.Background(), "topic", courier.QOSOne, false, msg); err != nil {
-					fmt.Printf("Publish() error = %s\n", err)
-				} else {
-					fmt.Println("Publish() success")
-				}
-			case <-stopCh:
-				tick.Stop()
-				return
-			}
+	func main() {
+		reg := prometheus.NewRegistry()
+		m := metrics.NewPrometheus()
+		if err := m.AddToRegistry(reg); err != nil {
+			panic(err)
 		}
-	}()
 
-	<-stopCh
+		c, err := courier.NewClient(
+				courier.WithUsername("username"),
+				courier.WithPassword("password"),
+				courier.WithTCPAddress("localhost", 1883),
+				courier.WithKeepAlive(10*time.Second),
+				courier.WithConnectTimeout(10*time.Second),
+				courier.WithWriteTimeout(2*time.Second),
+				courier.WithMaxReconnectInterval(5*time.Minute),
+				courier.WithGracefulShutdownPeriod(time.Minute),
+				courier.WithCustomMetrics(m),
+				// courier.WithPersistence(s),   // persistence for qos > 0 use-cases
+			)
 
-	stopCtx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancel()
-	_ = metricsServer.Shutdown(stopCtx)
-	c.Stop()
-}
+		if err != nil {
+			panic(err)
+		}
 
-```
+		metricsServer := http.Server{
+			Addr:              ":9090",
+			Handler:           promhttp.HandlerFor(reg, promhttp.HandlerOpts{}),
+		}
+		go func() {
+			_ = metricsServer.ListenAndServe()
+		}()
+
+		if err := c.Start(); err != nil {
+			panic(err)
+		}
+
+		stopCh := make(chan os.Signal)
+		signal.Notify(stopCh, []os.Signal{os.Interrupt, syscall.SIGTERM}...)
+
+		go func() {
+			tick := time.NewTicker(time.Second)
+			for {
+				select {
+				case t := <-tick.C:
+					msg := map[string]interface{}{
+						"time": t.UnixNano(),
+					}
+					if err := c.Publish(context.Background(), "topic", courier.QOSOne, false, msg); err != nil {
+						fmt.Printf("Publish() error = %s\n", err)
+					} else {
+						fmt.Println("Publish() success")
+					}
+				case <-stopCh:
+					tick.Stop()
+					return
+				}
+			}
+		}()
+
+		<-stopCh
+
+		stopCtx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+		_ = metricsServer.Shutdown(stopCtx)
+		c.Stop()
+	}
 */
 package courier // import "***REMOVED***"

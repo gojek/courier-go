818752273911bc2c4c5461c138fc84b8094ca3db
Swagat Parida <swagat.parida@gojek.com>
2022-04-02 15:39:55 +0530

    Move golang.org/x/net to inside xds gomod

--- xds/go.mod
+++ xds/go.mod
@@ -9,6 +9,7 @@ require (
 	google.golang.org/genproto v0.0.0-20200526211855-cb27e3aa2013
 	google.golang.org/grpc v1.36.0
 	google.golang.org/protobuf v1.27.1
+	golang.org/x/net v0.0.0-20210405180319-a5a99cb37ef4 // indirect
 )
 
 replace github.com/gojekfarm/courier-go => ../
--- xds/xdsclient/client_test.go
+++ xds/xdsclient/client_test.go
@@ -2,49 +2,46 @@ package xdsclient
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
+	corev3 "github.com/envoyproxy/go-control-plane/envoy/config/core/v3"
+	"github.com/gojekfarm/courier-go/xds/bootstrap"
+	structpb "github.com/golang/protobuf/ptypes/struct"
 	"os"
 	"os/signal"
 	"testing"
 )
 
 func TestClient_streamEndpoints(t *testing.T) {
-	//d, err := json.Marshal(&bootstrap.Config{XDSServer: &bootstrap.ServerConfig{
-	//	ServerURI: "localhost:9100",
-	//	NodeProto: &corev3.Node{
-	//		Id:      "52fdfc07-2182-454f-963f-5f0f9a621d72",
-	//		Cluster: "pusher",
-	//		Metadata: &structpb.Struct{Fields: map[string]*structpb.Value{
-	//			"APP": {Kind: &structpb.Value_StringValue{StringValue: "pusher"}},
-	//		}},
-	//		Locality: &corev3.Locality{
-	//			Region: "asia-east1",
-	//			Zone:   "asia-east1-a",
-	//		},
-	//	},
-	//}})
-	//if err != nil {
-	//	t.Error(err)
-	//}
-	//
-	//cfg, err := bootstrap.NewConfigFromContents(d)
-	//if err != nil {
-	//	t.Error(err)
-	//}
-	//
-	//c, err := New(cfg.XDSServer, "customer")
-	//
-	//fmt.Println(err)
+	d, err := json.Marshal(&bootstrap.Config{XDSServer: &bootstrap.ServerConfig{
+		ServerURI: "localhost:9100",
+		NodeProto: &corev3.Node{
+			Id:      "52fdfc07-2182-454f-963f-5f0f9a621d72",
+			Cluster: "pusher",
+			Metadata: &structpb.Struct{Fields: map[string]*structpb.Value{
+				"APP": {Kind: &structpb.Value_StringValue{StringValue: "pusher"}},
+			}},
+			Locality: &corev3.Locality{
+				Region: "asia-east1",
+				Zone:   "asia-east1-a",
+			},
+		},
+	}})
+	if err != nil {
+		t.Error(err)
+	}
 
-	ctx, f := signal.NotifyContext(context.Background(), os.Kill, os.Interrupt)
+	cfg, err := bootstrap.NewConfigFromContents(d)
+	if err != nil {
+		t.Error(err)
+	}
 
-	f()
+	c, err := New(cfg.XDSServer, "customer")
+
+	fmt.Println(err)
+
+	ctx, _ := signal.NotifyContext(context.Background(), os.Kill, os.Interrupt)
+
 	<- ctx.Done()
-
-	//c.Close()
-
-	fmt.Println("Soemthiang 1")
-	//
-	<- ctx.Done()
-	fmt.Println("Soemthiang")
+	c.Close()
 }

5c1b6a0e259b0c62ce44588e2ac38d3eee456af0
ajatprabha <ajat.prabha.leo@gmail.com>
2023-01-03 19:36:30 +0530

    Releasing v0.4.0

--- version.go
+++ version.go
@@ -2,6 +2,6 @@ package courier
 
 // Version can be used to get the current courier library version
 func Version() string {
-	return "0.3.1"
+	return "0.4.0"
 	// This string is updated by the pre_release.sh script during release
 }

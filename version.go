8888cda7e817b7f0cd6df74656f48729e2b8ede3
Paras Sharma <parassharma8041@gmail.com>
2025-12-12 11:16:31 +0530

    Releasing v0.7.12

--- version.go
+++ version.go
@@ -2,6 +2,6 @@ package courier
 
 // Version can be used to get the current courier library version
 func Version() string {
-	return "0.7.11"
+	return "0.7.12"
 	// This string is updated by the pre_release.sh script during release
 }

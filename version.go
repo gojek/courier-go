31349b98134a0c28ab9bfa2acaeef052afed81ed
Ken Kalang <kenkalang@yahoo.com>
2026-02-13 16:58:30 +0700

    Releasing v0.7.15

--- version.go
+++ version.go
@@ -2,6 +2,6 @@ package courier
 
 // Version can be used to get the current courier library version
 func Version() string {
-	return "0.7.14"
+	return "0.7.15"
 	// This string is updated by the pre_release.sh script during release
 }
